dfbf51acb97e81674b56256f6a98d836d2c5c770
cactus-y <youseokwon1667@gmail.com>
2023-07-19 06:04:18 +0000

    puppeteer fixed 2

--- src/menu/skku_suwon.js
+++ src/menu/skku_suwon.js
@@ -12,7 +12,10 @@ const getSkkuSuwonMenu = async(year, month, dayOfMonth) => {
             var url = `https://www.skku.edu/skku/campus/support/welfare_11_1.do?mode=info&srDt=${year}-${month}-${dayOfMonth}&srCategory=L&conspaceCd=20201${conSpaceList[idx]}&srResId=${resIdList[idx]}&srShowTime=D&srCategory=L`;
             
             // const html = await axios.get(url);
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
             const page = await browser.newPage();
 
             // page.on('dialog', async dialog => {
